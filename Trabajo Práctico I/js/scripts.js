aaf1144ac3f609fbe001678650da2405d6b15b3c
bara1422 <xxbaraxx@hotmail.com>
2025-04-07 23:19:19 -0300

    add navbar, footer y pagina servicios

--- Trabajo Práctico I/js/scripts.js
+++ Trabajo Práctico I/js/scripts.js
@@ -5,46 +5,48 @@ document.addEventListener("DOMContentLoaded", function() {
         const alertDialog = document.querySelector("#alert-dialog");
         alertDialog.showModal();
     });
+});
 
-    const carrusel = document.querySelector("#servicios-ul");
-    const antBtn = document.querySelector("#botonAnterior");
-    const sigBtn = document.querySelector("#sigBtn");
-    const anchoCard = document.querySelector(".card-exterior-li").offsetWidth;
 
-    let posicion = 0;
+const carrusel = document.querySelector("#servicios-ul");
+const antBtn = document.querySelector("#botonAnterior");
+const sigBtn = document.querySelector("#sigBtn");
+const anchoCard = document.querySelector(".card-exterior-li").offsetWidth;
+
+let posicion = 0;
+
+antBtn.disabled = true;
+
+antBtn.addEventListener("click", function() {
+    posicion -= anchoCard;
+    console.log(posicion)
+    if (posicion <= 8) {
+        posicion = 0;
+        antBtn.disabled = true;
+    }
+    sigBtn.disabled = false;
     
-   antBtn.disabled = true;
-    
-    antBtn.addEventListener("click", function() {
-        posicion -= anchoCard;
-        console.log(posicion)
-        if (posicion <= 8) {
-            posicion = 0;
-            antBtn.disabled = true;
-        }
-        sigBtn.disabled = false;
-      
-        carrusel.scrollTo({
-            left: posicion,
-            behavior: "smooth"
-        });
-    });
-
-    sigBtn.addEventListener("click", function() {
-        posicion += anchoCard;
-        console.log(posicion)
-        if (posicion > 0) {
-            antBtn.disabled = false;
-        }
-        
-        if (posicion >= anchoCard * 3) {
-            posicion = anchoCard * 3;
-            sigBtn.disabled = true;
-        }
-        
-        carrusel.scrollTo({
-            left: posicion,
-            behavior: "smooth"
-        });
+    carrusel.scrollTo({
+        left: posicion,
+        behavior: "smooth"
     });
 });
+
+sigBtn.addEventListener("click", function() {
+    posicion += anchoCard;
+    console.log(posicion)
+    if (posicion > 0) {
+        antBtn.disabled = false;
+    }
+    
+    if (posicion >= anchoCard * 3) {
+        posicion = anchoCard * 3;
+        sigBtn.disabled = true;
+    }
+    
+    carrusel.scrollTo({
+        left: posicion,
+        behavior: "smooth"
+    });
+});
+
