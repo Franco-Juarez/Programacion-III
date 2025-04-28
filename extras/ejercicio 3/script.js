e89064a9637d24071cf4e28eaeb72abdeb43e6c8
luucanclini <luucanclini@gmail.com>
2025-04-28 12:21:20 -0300

    .

--- extras/ejercicio 3/script.js
+++ extras/ejercicio 3/script.js
@@ -4,9 +4,22 @@ const cursiva = document.getElementById("cursiva");
 const subrayado = document.getElementById("subrayado");
 const texto = document.getElementById("texto");
 let color = document.getElementById("color");
-let numero = document.getElementById("number");
+let numero = document.getElementById("fuente");
 let aplicar = document.getElementById("aplicar");
 
 negrita.addEventListener("click", () => {
-    texto.style.fontWeight = "bold";
+    texto.classList.toggle("bold");
+});
+
+cursiva.addEventListener("click", () => {
+    texto.classList.toggle("italic");
+});
+
+subrayado.addEventListener("click", () => {
+    texto.classList.toggle("underline");
+});
+
+aplicar.addEventListener("click", () => {
+    texto.style.color = color.value;
+    texto.style.fontSize = numero.value + "px";
 });
