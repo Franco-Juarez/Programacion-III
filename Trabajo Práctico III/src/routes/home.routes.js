71df3ad9762d23545ee6f2e4e4b65ec31f77897c
bara1422 <xxbaraxx@hotmail.com>
2025-05-30 23:15:56 -0300

    ruta home /home

--- Trabajo Práctico III/src/routes/home.routes.js
+++ Trabajo Práctico III/src/routes/home.routes.js
@@ -1,7 +1,7 @@
 const {Router} = require('express');
 const {home} = require('../controllers/home/home.controller.js')
 const rutaHome = Router();
-rutaHome.get('/', home);
+rutaHome.get('/home', home);
 //Otras rutas CRUD
 
 module.exports = rutaHome;
