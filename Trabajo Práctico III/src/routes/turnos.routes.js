08b193a98b4fc1335046c51d355e66bd9c68d61b
luucanclini <luucanclini@gmal.com>
2025-06-05 16:31:45 -0300

    creacion d carpeta schemas y turno.schema.js

--- Trabajo Práctico III/src/routes/turnos.routes.js
+++ Trabajo Práctico III/src/routes/turnos.routes.js
@@ -3,11 +3,15 @@ const turnosController = require('../controllers/API/turnos.controller.js')
 const {
   verifyTokenMiddleware,
 } = require("../middlewares/verifyToken.middleware");
+const validate = require('../middlewares/validacionTurno.middleware.js');
+const turnoSchema = require('../schemas/turno.schema.js');
+
 const rutaTurnos = Router();
-rutaTurnos.post('/',verifyTokenMiddleware, turnosController.create);
+
+rutaTurnos.post('/',verifyTokenMiddleware, validate(turnoSchema.create) ,turnosController.create);
 rutaTurnos.get('/',verifyTokenMiddleware, turnosController.getTurnos);
-rutaTurnos.get('/:id',verifyTokenMiddleware, turnosController.getByPacienteId);
-//rutaTurnos.put('/:id',verifyTokenMiddleware, turnosController.update);
-rutaTurnos.delete('/:id',verifyTokenMiddleware, turnosController.deleteTurno);
+rutaTurnos.get('/:id',verifyTokenMiddleware, validate(turnoSchema.idParam, 'params') ,turnosController.getByPacienteId);
+rutaTurnos.put('/:id',verifyTokenMiddleware, validate(turnoSchema.idParam, 'params'), validate(turnoSchema.update),turnosController.update);
+rutaTurnos.delete('/:id',verifyTokenMiddleware, validate(turnoSchema.idParam, 'params'),turnosController.deleteTurno);
 
 module.exports = rutaTurnos;
