1217e311861f20472c605cb779dedda36c9cbfed
luucanclini <luucanclini@gmal.com>
2025-06-06 16:02:06 -0300

    creacion d paciente.schema.js

--- Trabajo Práctico III/src/routes/pacientes.routes.js
+++ Trabajo Práctico III/src/routes/pacientes.routes.js
@@ -4,12 +4,16 @@ const {
   verifyTokenMiddleware,
 } = require("../middlewares/verifyToken.middleware.js");
 const rutaPacientes = Router();
+
+const validate = require("../middlewares/validate.middleware.js");
+const pacienteSchema = require("../schemas/paciente.schema.js");
+
 rutaPacientes.get("/", verifyTokenMiddleware, pacientesController.list);
-rutaPacientes.post('/login',pacientesController.login)
-rutaPacientes.get("/:id", verifyTokenMiddleware, pacientesController.getById);
-rutaPacientes.post("/", verifyTokenMiddleware, pacientesController.create);
-rutaPacientes.put("/:id", verifyTokenMiddleware, pacientesController.update);
-rutaPacientes.delete("/:id", verifyTokenMiddleware, pacientesController.delete);
+rutaPacientes.post('/login', validate(pacienteSchema.login, "body"),pacientesController.login)
+rutaPacientes.get("/:id", verifyTokenMiddleware, validate(pacienteSchema.getById, "params"), pacientesController.getById);
+rutaPacientes.post("/", verifyTokenMiddleware, validate(pacienteSchema.create, "body"), pacientesController.create);
+rutaPacientes.put("/:id", verifyTokenMiddleware,validate(pacienteSchema.getById, "params"), validate(pacienteSchema.update, "body"), pacientesController.update);
+rutaPacientes.delete("/:id", verifyTokenMiddleware, validate(pacienteSchema.getById, "params"), pacientesController.delete);
 
 //Otras rutas CRUD
 
