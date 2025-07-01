ef254d67be9ee9310b330c1f1a60789ee04db6fe
Franco Juarez <franjuaache@gmail.com>
2025-07-01 20:58:44 -0300

    Ajuste en el frontend

--- Proyecto Final/frontend/src/App.js
+++ Proyecto Final/frontend/src/App.js
@@ -7,71 +7,19 @@ import BookFilter from './components/ui/bookFilter';
 function App() {
 
   const [booksCount, setBooksCount] = useState(3);
-  const [books, setBooks] = useState([
-    {
-      id: 1,
-      title: 'El Principito',
-      author: 'Antoine de Saint-Exupéry',
-      status: 'to read',
-      rating: 5,
-      genre: 'Fiction',
-      pages: 96,
-      year: 1943,
-    },
-    {
-      id: 2,
-      title: 'Cien años de soledad',
-      author: 'Gabriel García Márquez',
-      status: 'reading',
-      rating: 5,
-      genre: 'Magical Realism',
-      pages: 417,
-      year: 1967,
-    },
-    {
-      id: 3,
-      title: '1984',
-      author: 'George Orwell',
-      status: 'read',
-      rating: 4,
-      genre: 'Dystopian',
-      pages: 328,
-      year: 1949,
-    },
-    {
-      id: 4,
-      title: 'Sapiens: De animales a dioses',
-      author: 'Yuval Noah Harari',
-      status: 'to read',
-      rating: 4,
-      genre: 'Non-fiction',
-      pages: 464,
-      year: 2011,
-    },
-    {
-      id: 5,
-      title: 'Rayuela',
-      author: 'Julio Cortázar',
-      status: 'read',
-      rating: 5,
-      genre: 'Experimental Fiction',
-      pages: 601,
-      year: 1963,
-    },
-  ]);
-
+  const [books, setBooks] = useState([]);
 
   useEffect(() => {
-    const fetchBooksCount = async () => {
+    const fetchBooks = async () => {
       const response = await fetch('http://localhost:3001/api/libros');
       if (!response.ok) {
         console.error('Error al obtener libros');
         return;
       }
       const data = await response.json();
-      // setBooks(data.books);
+      setBooks(data);
     };
-    fetchBooksCount();
+    fetchBooks();
   }, []);
 
   return (
--- Proyecto Final/frontend/src/components/ui/bookCard.jsx
+++ Proyecto Final/frontend/src/components/ui/bookCard.jsx
@@ -1,13 +1,31 @@
 const BookCard = ({ book }) => {
     return (
         <div className="book-card">
-            <h3>{book.title}</h3>
-            <p>{book.author}</p>
-            <p>{book.genre}</p>
-            <p>{book.year}</p>
-            <p>{book.pages} páginas</p>
-            <p>Calificación: {book.rating}</p>
-            <p>Estado: {book.status}</p>
+            <h3>{book.titulo}</h3>
+            <p>{book.autor}</p>
+            <div className="book-details">
+                <p>{book.genero}</p>
+                <p>
+                    {
+                        book.estado === 'unread' ? 
+                        <span className="status unread">No leído</span> :
+                        book.estado === 'reading' ?
+                        <span className="status reading">Leyendo</span> :
+                        book.estado === 'read' ?
+                        <span className="status read">Leído</span> :
+                        null
+                    }
+                </p>
+            </div>
+            <p>{book.anioPublicacion}</p>
+            <p>Calificación: 
+            {
+                book.calificacion ? 
+                <span className="rating">{book.calificacion}</span> : 
+                <span className="rating"> No calificado</span>
+            }
+            </p>
+            <p className="book-description">Descripción: {book.descripcion}</p>
         </div>
     );
 }
